92e8d56d456fb186edae6664fe0b6a3e895e4063
caiweiqiang <1429388864@qq.com>
2022-06-16 20:41:03 +0800

    添加路由守卫

--- src/route/router.ts
+++ src/route/router.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store/store'
 
 const routes = [
   {
@@ -9,7 +10,10 @@ const routes = [
   {
     path: '/create',
     name: 'create',
-    component: () => import('@/views/createPost.vue')
+    component: () => import('@/views/createPost.vue'),
+    meta: {
+      requiredLogin: true
+    }
   },
   {
     path: '/column/:id',
@@ -19,12 +23,28 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import('@/views/login.vue')
+    component: () => import('@/views/login.vue'),
+    meta: { redirectAlreadyLogin: true }
   }
 ]
+
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+// 路由前置守卫
+router.beforeEach((to, form, next) => {
+  console.log('to', to.meta)
+  console.log(store.state.user.isLogin)
+  if (to.meta.requiredLogin && !store.state.user.isLogin) {
+    console.log('没有登录')
+    next({ path: '/login' })
+  } else if (to.meta.redirectAlreadyLogin && store.state.user.isLogin) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
